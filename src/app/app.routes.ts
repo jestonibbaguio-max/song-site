c7efc6e5aa3a8a56b1e0257eca20005ea1702c36
Irang <robin.jay.irang@accenture.com>
2026-05-21 10:35:38 +0800

    Update People Lead

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -116,5 +116,10 @@ export const routes: Routes = [
     path: 'my-update-contact',
     loadComponent: () =>
       import('./song-bench/my-journey/task/my-update-contact/my-update-contact').then(m => m.MyUpdateContact)
+  },
+  {
+    path: 'people-lead',
+    loadComponent: () =>
+      import('./song-bench/my-journey/task/people-lead/people-lead').then(m => m.PeopleLead)
   }
 ];
